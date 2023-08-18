9d98b5466a64b4f929dbdd66be085d94cd679f88
kkangh00n <kig2454@gmail.com>
2023-08-18 13:11:53 +0900

    feat: 검색 조건 쿼리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -74,5 +74,34 @@ public class QuerydslBasicTest {
         Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
+    @Test
+    public void search(){
+        queryFactory.selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+//                                .and(member.age.eq(10))
+                )
+                .fetchOne();
+    }
+
+    /**
+     * member.username.eq("member1") // username = 'member1'
+     * member.username.ne("member1") //username != 'member1'
+     * member.username.eq("member1").not() // username != 'member1'
+     * member.username.isNotNull() //이름이 is not null
+     * member.age.in(10, 20) // age in (10,20)
+     * member.age.notIn(10, 20) // age not in (10, 20)
+     * member.age.between(10,30) //between 10, 30
+     * member.age.goe(30) // age >= 30
+     * member.age.gt(30) // age > 30
+     * member.age.loe(30) // age <= 30
+     * member.age.lt(30) // age < 30
+     * member.username.like("member%") //like 검색
+     * member.username.contains("member") // like ‘%member%’ 검색
+     * member.username.startsWith("member") //like ‘member%’ 검색
+     * ...
+     */
+
 
 }
