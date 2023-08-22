ab5e3073ca498463b6e674fd0359ef2f6dbd36b2
h00ny_Island <102799700+kkangh00n@users.noreply.github.com>
2023-08-22 20:46:54 +0900

    실무 활용 - 순수 JPA와 Querydsl (#4)
    * feat: JPA와 QueryDsl 비교
    
    * feat: 동적 쿼리와 성능 최적화 조회 - Builder 사용
    
    * feat: 동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용
    
    * feat: 조회 API 컨트롤러 개발

---
+++ src/main/java/study/querydsl/dto/MemberTeamDto.java
@@ -0,0 +1,23 @@
+package study.querydsl.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+//Member와 소속 Team의 정보 응답 DTO
+@Data
+public class MemberTeamDto {
+    private Long memberId;
+    private String username;
+    private int age;
+    private Long teamId;
+    private String teamName;
+
+    @QueryProjection
+    public MemberTeamDto(Long memberId, String username, int age, Long teamId, String teamName){
+        this.memberId = memberId;
+        this.username = username;
+        this.age = age;
+        this.teamId = teamId;
+        this.teamName = teamName;
+    }
+}
